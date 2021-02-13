d186835b20ccf214a209bb268f000ff4a2ce17e1
Hiram <hiramg90@gmail.com>
2021-02-13 08:53:47 -0600

    Visualizing Route Changes

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import NProgress from 'nprogress';
+import Router from "next/router";
 import Page from "../components/Page";
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+import 'nprogress/nprogress.css';
+import '../components/styles/nprogress.css';
 
 const MyApp = ({Component, pageProps}) => {
   return (
