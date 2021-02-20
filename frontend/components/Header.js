108ccef038010d2a45c96f578d09f15bf748a791
Hiram <hiramg90@gmail.com>
2021-02-20 11:46:37 -0600

    Fixing and Styling the Nav

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -26,7 +26,7 @@ const HeaderStyles = styled.header`
     display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
-    align-items: center;
+    align-items: stretch;
   }
 
   .sub-bar {
@@ -40,16 +40,14 @@ const Header = () => {
   return (
     <HeaderStyles>
       <div className="bar">
-
         <Logo>
           <Link href="/">Sick fits</Link>
         </Logo>
-
+        <Nav/>
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
-      <Nav/>
     </HeaderStyles>
   );
 };
