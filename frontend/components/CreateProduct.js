4e0083b4fda565c9e5691020993d9315913ad6c5
Hiram <hiramg90@gmail.com>
2021-02-21 08:46:46 -0600

    Refetching queries after a successful mutation

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -3,6 +3,7 @@ import useForm from "../lib/useForm";
 import Form from "./styles/Form";
 import {gql, useMutation} from "@apollo/client";
 import DisplayError from "./ErrorMessage";
+import {ALL_PRODUCTS_QUERY} from "./Products";
 
 const CREATE_PRODUCT_MUTATION = gql`
   mutation CREATE_PRODUCT_MUTATION(
@@ -43,6 +44,9 @@ const CreateProduct = () => {
 
   const [createProduct, {loading, error, data}] = useMutation(CREATE_PRODUCT_MUTATION, {
     variables: inputs,
+    refetchQueries: [
+      {query: ALL_PRODUCTS_QUERY},
+    ],
   });
 
   //console.log(createProduct);
