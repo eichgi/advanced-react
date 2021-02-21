cc01569e122231dfe7f088a4e1bc5113d682c6a6
Hiram <hiramg90@gmail.com>
2021-02-21 08:30:33 -0600

    Creating products via our Mutations

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,6 +1,36 @@
 import React, {useState} from 'react';
 import useForm from "../lib/useForm";
 import Form from "./styles/Form";
+import {gql, useMutation} from "@apollo/client";
+import DisplayError from "./ErrorMessage";
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    #variables
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(data: {
+      name: $name
+      description: $description
+      price: $price
+      status: "AVAILABLE"
+      photo: {
+        create: {
+          image: $image
+          altText: $name
+        }
+      }
+    }) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 const CreateProduct = () => {
 
@@ -11,15 +41,29 @@ const CreateProduct = () => {
     description: 'The best thing ever'
   });
 
-  const handleSubmit = (e) => {
+  const [createProduct, {loading, error, data}] = useMutation(CREATE_PRODUCT_MUTATION, {
+    variables: inputs,
+  });
+
+  //console.log(createProduct);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(inputs);
+    //console.log(inputs);
+
+    //Submit input field
+    await createProduct();
+    console.log("Response data: ", data);
+    clearForm();
   };
 
   return (
     <Form onSubmit={handleSubmit}>
-      <fieldset>
+
+      <DisplayError error={error}/>
+
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
           <input type="file"
