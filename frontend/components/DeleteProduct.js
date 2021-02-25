ac9185eef4ce542fe306fb12082c6f3ac6e6b735
Hiram <hiramg90@gmail.com>
2021-02-25 08:58:10 -0600

    Evicting items from the Apollo Cache

--- frontend/components/DeleteProduct.js
+++ frontend/components/DeleteProduct.js
@@ -10,10 +10,18 @@ const DELETE_PRODUCT_MUTATION = gql`
   }
 `;
 
+const update = (cache, payload) => {
+  //console.log(payload);
+  //console.log("running update function after delete");
+
+  cache.evict(cache.identify(payload.data.deleteProduct));
+};
+
 const DeleteProduct = ({id, children}) => {
 
   const [deleteProduct, {loading, error}] = useMutation(DELETE_PRODUCT_MUTATION, {
     variables: {id},
+    update,
   });
 
   const deleteHandler = () => {
